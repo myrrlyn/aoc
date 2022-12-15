02362f6ce119bc607e8eb684a4bef9929be62337
myrrlyn <self@myrrlyn.dev>
2022-12-15 11:28:21 -0600

    Solve 2022-12-14

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -11,3 +11,18 @@ macro_rules! sample {
         include_str!("../../sample.txt")
     };
 }
+
+pub trait Puzzle {
+    type Input;
+    type State;
+    type ParseError<'a>;
+    type ComputeError;
+
+    fn parse(input: &str) -> Result<Self::Input, Self::ParseError<'_>>;
+
+    fn prepare_state(input: Self::Input) -> Result<Self::State, Self::ComputeError>;
+
+    fn part_1(state: &mut Self::State) -> Result<i64, Self::ComputeError>;
+
+    fn part_2(state: &mut Self::State) -> Result<i64, Self::ComputeError>;
+}
